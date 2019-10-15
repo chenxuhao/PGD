c95fe0f6d034332f2e879352ff8bafd63bb9406a
chenxuhao <chen_xuhao@126.com>
2019-10-15 16:44:16 -0500

    fix error

--- graphlet_utils.cpp
+++ graphlet_utils.cpp
@@ -36,11 +36,7 @@ using namespace std;
 
 bool fexists(const char *filename) {
     ifstream ifile(filename);
-#ifdef WIN32
     return ifile!=0;
-#else
-    return ifile;
-#endif
 }
 
 #if defined(_WIN32) || defined(_WIN64)
